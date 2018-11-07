3a1eb761f524cd274f446feb99cc4c9abc9346a6
Fedoryuk-E <fedoryuk.yeb@gmail.com>
2018-11-07 13:09:45 +0200

    second

--- app/src/main/java/com/example/gazik/barcode/MainActivity.java
+++ app/src/main/java/com/example/gazik/barcode/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*sd*/
 
-        /*dsfsdddf test cosddmmist*/
+        /*dsfsdddf test codsddmmist*/
     }
 }
